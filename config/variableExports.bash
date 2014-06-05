581b25a38d48c51c86a6f6402a2f4c2ded8e72ea
Piotr Regula <regula.piotrek@gmail.com>
2014-06-05 11:23:16 +0200

    variable exports

---
+++ config/variableExports.bash
@@ -0,0 +1,5 @@
+export GITHUB="https://https://github.com/"
+export MYGITHUB="https://https://github.com/piotr-regula/"
+export WRLING102="wrling102.emea.nsn-net.net"
+export RWRLING102="regula@wrling102.emea.nsn-net.net"
+
